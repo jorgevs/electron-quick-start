b98ed8399c659ba5038f92e9c5383616d7cef462
jorgevs.mx <jorgevs.mx@gmail.com>
2019-06-29 16:32:57 -0400

    Formatting code

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const colors = require('colors');
-
-console.log(colors.rainbow('Hello world!'));
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow, secondaryWindow;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   console.log(">>> Creating window");
 
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
-    webPreferences: {nodeIntegration: true}
+    webPreferences: { nodeIntegration: true }
   });
+
   secondaryWindow = new BrowserWindow({
     width: 600, height: 300,
-    webPreferences: {nodeIntegration: true},
+    webPreferences: { nodeIntegration: true },
     parent: mainWindow,
     modal: true,
     show: false
@@ -28,9 +27,9 @@ function createWindow () {
   mainWindow.loadFile('index.html');
   secondaryWindow.loadFile('secondary.html');
 
-  setTimeout( () => {
+  setTimeout(() => {
     secondaryWindow.show();
-    setTimeout( () => {
+    setTimeout(() => {
       secondaryWindow.close();
       secondaryWindow = null;
     }, 3000)
@@ -52,22 +51,23 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-  console.log("Hi");
   createWindow();
 });
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 });
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+  if (mainWindow === null) {
+    createWindow()
+  }
 });
+
+console.log(colors.rainbow('Hello world!'));
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
